fc597737369b1b285702db8351eb8d3886f992ae
Cynthia Sarafianou <cynthiasarafianou@gmail.com>
2016-03-28 14:12:12 +0200

    addind welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'pages/welcome'
+
+  root 'pages#welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
